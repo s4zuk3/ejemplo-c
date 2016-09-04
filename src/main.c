6a99bf4ac37687e808e61aa5653c72edcc04d99c
Pablo Ulloa <pablo.ulloac@usach.cl>
2016-09-03 21:15:46 -0300

    Ejemplo

--- src/main.c
+++ src/main.c
@@ -2,20 +2,13 @@
 #include "basics.h"
 
 int main(int argc, char* argv[]){
-	int x=atoi(argv[1]),y=atoi(argv[2]);
-	Matriz m = nuevaMatriz(x,y);
-	mostrarMatriz(m);
+	String nombreArchivo=argv[1];
+	String buscar=argv[2];
+	printf("El archivo a leer es: %s\n\n",nombreArchivo);
+	String contenido=fileContents(nombreArchivo);
 	flag;
-	int i,j;
-	for(i=0;i<m.filas;i++){
-		for(j=0;j<m.columnas;j++){
-			m.casillas[i][j]=i+j;
-		}
-	}
-	mostrarMatriz(m);
-	flag;
-	deleteMatriz(m);
-	m=nuevaMatriz(y,x);
-	mostrarMatriz(m);
+	printf("%s\n", contenido);
+	int repeticiones=str_repetition(contenido,buscar);
+	printf("Se encontro %d veces la palabra %s\n", repeticiones,buscar);
 	exit(0);
 }
